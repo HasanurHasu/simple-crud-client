aa1c6b4343ebcf499c530c00a19c8c9d0db8cec1
Md. Hasanur Rahman <hasanurhasu121@gmail.com>
2023-10-14 09:28:01 +0600

    first commit

---
+++ src/components/Users.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+
+const Users = () => {
+    const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers);
+
+    const handleDeleteUser = _id =>{
+        console.log(_id);
+        fetch(`http://localhost:5000/users/${_id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.deletedCount>0){
+                alert('user delete successfully')
+
+                const remaining = users.filter(user => user._id !== _id);
+                setUsers(remaining);
+            }
+        })
+    }
+    return (
+        <div>
+            <p>{users.length}</p>
+            <div>
+                {
+                    users.map(user => <p key={user._id}>Name: {user.name} Email: {user.email} <button 
+                    onClick={() => handleDeleteUser(user._id)}
+                    >x</button> </p>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Users;
